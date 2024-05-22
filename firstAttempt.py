e4e6651798f1f674a240dded9e785a5e6d6a2142
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-05-22 12:14:09 -0600

    pagination fixed

--- firstAttempt.py
+++ firstAttempt.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = "https://www.goodwillfinds.com/search/?q=camera"
+url = "https://www.goodwillfinds.com/search/?q=camera&sz=200&start=0"
 response = requests.get(url)
 
 # parsing the HTML content
@@ -20,11 +20,14 @@ for product in products:
     title = title_element.text.strip() if title_element else "N/A"
     
     # Extract the product price
-    price_element = product.find('span', class_='b-price-item m-new')
+    price_element = product.find('span', class_='b-price-item')
     price = price_element.text.strip() if price_element else "N/A"
+
+    sale_element = product.find('span', class_='b-price-discount')
+    sale = sale_element.text.strip() if sale_element else "N/A"
     
     # Append the product data to the list
-    product_data.append({"Title": title, "Price": price})
+    product_data.append({"Title": title, "Price": price, "Sale": sale})
 
 # Convert the list to a pandas DataFrame
 df = pd.DataFrame(product_data)
